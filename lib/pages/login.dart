9695ce8b45e915a41257a4c27b0b214b6e7cdfea
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2020-08-14 20:32:21 +0530

    code

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:outline_gradient_button/outline_gradient_button.dart';
+
+
+
+
 class Loginpage extends StatefulWidget {
   @override
   _LoginpageState createState() => _LoginpageState();
@@ -9,86 +14,122 @@ class _LoginpageState extends State<Loginpage>{
 bool _checkBoxVal=true;
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      resizeToAvoidBottomPadding:false ,
-        body: new Stack(fit: StackFit.expand,
-            children: <Widget>[
-          new Image(
-            image: new AssetImage('images/login/background.jpg'),
-            fit: BoxFit.cover,
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height,
+            width: size.width,
+            child: new Image.asset(
+              'images/login/background.jpg',
+              width: size.width,
+              height: size.height,
+              fit: BoxFit.fitHeight,
+            ),
           ),
-          Expanded(
-            child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('images/login/logo.png',
-height: 250.0,
-                    width: 150.0,
-                  ),
-                  new Form(
-                      child: Theme(
-                        data: new ThemeData(
-                          brightness: Brightness.dark,
-                          primarySwatch: Colors.orange,
-                          inputDecorationTheme: new InputDecorationTheme(
-                              labelStyle:
-                              new TextStyle(color: Colors.white, fontSize: 20.0)),
-                        ),
-                        child: Container(
-                          padding: const EdgeInsets.all(40.0),
-                          child: new Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                new TextFormField(
-                                  decoration: new InputDecoration(
-                                    prefixIcon: Icon(Icons.mail),
-                                    labelText: "Employee ID",
-                                  ),
-                                  keyboardType: TextInputType.emailAddress,
-                                ),
-                                new TextFormField(
-                                  decoration: new InputDecoration(
-                                    prefixIcon: Icon(Icons.lock),
-                                    suffixIcon: Icon(Icons.remove_red_eye,),
-                                    labelText: "Password",
-                                  ),
+          Positioned(
+            top: 100,
+            right: 20,
+            child: Center(
+              child: Stack(children: <Widget>[
 
-                                  keyboardType: TextInputType.text,
-                                  obscureText: true,
-                                ),
-                                Row(
-                                  children: [
-                                    new Text('Remember Password',style:TextStyle(color:Colors.white)),
-                                    Checkbox(
-                                      hoverColor: Colors.white,
-                                      onChanged: (bool value)
-                                      {
-                                        setState(()=>this._checkBoxVal=value);
-                                      },
-                                      value: this._checkBoxVal,
+                 Image.asset('images/login/logo.png' ,width: 200)
+              ],
+              ),
+            ),
+          ),
+
+
+
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,250,0,0),
+            child: Container(
+              width: double.infinity,
+              child: Center(
+                child: Stack(
+                  children: <Widget>[
+                    new Container(
+                        child: Theme(
+                          data: new ThemeData(
+                            brightness: Brightness.dark,
+                            primarySwatch: Colors.red,
+                            inputDecorationTheme: new InputDecorationTheme(
+                                labelStyle:
+                                new TextStyle(color: Colors.white,)),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(20.0),
+                            child: new Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  new TextFormField(
+                                    decoration: new InputDecoration(
+                                      prefixIcon: Icon(Icons.mail),
+                                      labelText: "Employee ID",
+
                                     ),
-                                    new Text('Forget Password',style:TextStyle(color:Colors.white,)),
-
-                                  ],
-                                ),
-                                new MaterialButton(
-                                  height: 20.0,
-                                  minWidth: 40.0,
-                                  textColor: Colors.white,
-                                  child: new Image.asset('images/login/login-btn.png',
-                                    height: 210.0,
-                                    width: 105.0,
+                                    keyboardType: TextInputType.emailAddress,
                                   ),
-                                  onPressed: () => {},
-                                  splashColor: Colors.red,
-                                )
-                              ]),
-                        ),
-                      ))
-                ]),
-          )
-        ]));
+                                  new TextFormField(
+                                    decoration: new InputDecoration(
+                                      prefixIcon: Icon(Icons.lock),
+                                      suffixIcon: Icon(Icons.remove_red_eye,),
+                                      labelText: "Password",
+                                    ),
+
+                                    keyboardType: TextInputType.text,
+                                    obscureText: true,
+                                  ),
+
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+
+                                      Row(
+                                        children: <Widget>[
+                                          new Text('Remember Password',style:TextStyle(color:Colors.white,fontSize: 12)),
+                                          Checkbox(
+                                            hoverColor: Colors.red,
+                                            onChanged: (bool value)
+                                            {
+                                              setState(()=>this._checkBoxVal=value);
+                                            },
+                                            value: this._checkBoxVal,
+                                          ),
+                                        ],
+                                      ),
+
+                                      new Text('Forget Password',style:TextStyle(color:Colors.white,fontSize: 12)),
+
+                                    ],
+                                  ),
+                                  SizedBox(height: 20,),
+                                  OutlineGradientButton(
+                                    padding: const EdgeInsets.fromLTRB(30,15,30,15),
+
+                                    backgroundColor: Colors.white,
+                                    child: Text('LOGIN',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.red),),
+                                    gradient: LinearGradient(colors: [Colors.red, Colors.red[900]]),
+                                    strokeWidth: 8,
+                                    radius: Radius.circular(20),
+                                  ),
+                                ]),
+                          ),
+                        ))
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
-
 
